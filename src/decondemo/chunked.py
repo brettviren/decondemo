06e34ac1840ee0a65fc477e0a76c82a747f32d39
Brett Viren <brett.viren@gmail.com>
2025-10-26 08:39:38 -0400

    feat: Add time step generators and event latching classes

--- src/decondemo/chunked.py
+++ src/decondemo/chunked.py
@@ -22,16 +22,127 @@ results.  The graph is composed through calls to the callable method.
 
 '''
 import random
+import numpy as np
 
+class ExpoTime:
+    def __init__(self, rate=1.0):
+        self.rate = rate
+    def __call__(self):
+        return random.expovariate(self.rate)
+
+class UniformTime:
+    def __init__(self, rate=1.0):
+        self.rate = rate
+    def __call__(self):
+        return 1.0/rate
 
 class TimeSource:
-    def __init__(self, rate=1.0, start=0.0):
+    '''
+    A generator of decay times.
+    '''
+    def __init__(self, step, start=0.0, limit=100):
+        '''
+        Create a time source starting at "start" producing times via a
+        "step" callable.  Each call to step is expected to return a relative
+        time value that will advance the time source's time.
+
+        Exit after emitting "limit" number of times.
+        '''
+        self.step = step
         self.now = start
-        self.rate = rate
+        self.limit = limit
 
     def __call__(self):
         while True:
+            if not self.limit:
+                return
+            self.limit -= 1
             dt = random.expovariate(self.rate)
             self.now += dt
             yield self.now
-    
+
+class Latch:
+    '''
+    Produce a discrete sampled impulse train from times
+    '''
+    def __init__(self, sample_period=1.0, chunk_size=100, self.start_time=0.0):
+        '''
+        Latch input times with given sample period and emit arrays of chunk size.
+        '''
+        self.sample_period = sample_period
+        self.chunk_size = chunk_size
+        self.now = start_time
+        self.chunk = None
+        self.fresh()            # ignore old on first call
+
+    def fresh(self):
+        '''
+        Initialize a fresh chunk, return the old chunk.
+        '''
+        old = self.chunk
+        self.chunk = np.zeros(self.chunk_size, dtype=float)
+        return old
+
+    @property
+    def duration(self):
+        '''
+        The duration of a chunk.
+        '''
+        return self.sample_period * self.chunk_size
+
+    @property
+    def later(self):
+        '''
+        The smallest past this chunk.
+        '''
+        return self.now + self.duration
+        
+    def contains(self, time):
+        '''
+        Return true if time is in the current chunk.
+        '''
+        return self.now <= time and time < self.now + self.duration
+
+    def tick(self, time):
+        '''
+        Return the index for the time given current now time.
+
+        This may be outside the bounds of the current chunk.  It will be
+        negative if "time" is before "now".
+        '''
+        return int( (time - self.now ) / sample_period )
+
+    def latch(self, time, value=1.0):
+        '''
+        Add value the array at tick corresponding to time.
+
+        Raise IndexError if time is out-of-bounds.
+        '''
+        ind = self.tick(time)
+        if ind < 0 or ind >= self.chunk_size:
+            raise IndexError(f'ind {ind} out of bounds for size {self.chunk_size}')
+        array[ind] += alue
+
+    def emit(self):
+        '''
+        Yield current chunk and advance one duration.
+        '''
+        done = self.fresh()
+        self.now += self.duration
+        yield done
+
+    def __call__(self, time_source):
+        '''
+        Emit chunks of latched time
+        '''
+        for time in time_source:
+            if time < self.now:
+                raise ValueError(f'time violation {time} < {self.now}')
+
+            # Emit chunks until we catch up
+            while time >= self.later:
+                time -= self.duration
+                self.emit()
+                
+            self.latch(time)
+            
